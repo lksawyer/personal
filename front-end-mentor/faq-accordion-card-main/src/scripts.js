b8b4889850260d845bdee62e62d7f0a89b91e290
lksawyer <loganksawyer@gmail.com>
2021-05-19 17:50:17 -0400

    styles

--- front-end-mentor/faq-accordion-card-main/src/scripts.js
+++ front-end-mentor/faq-accordion-card-main/src/scripts.js
@@ -1,3 +1,40 @@
-$(".c-fold").click(function () {
-  $("this").addClass("is-active");
+$(document).ready(function () {
+  console.log("scripts loaded");
+
+  // Get the height of all e.currentTarget.children[2]
+  // Store these values
+  // Set e.currentTarget.children[2] css height property to 0
+
+  $(".c-fold").click(function (e) {
+    console.log(".c-fold clicked");
+    console.log("e :", e);
+    let active = is_active(e);
+
+    // Determine whether or not .is-active should be removed or added
+    if (active) {
+      remove_active(e);
+    } else {
+      add_active(e);
+    }
+  });
 });
+
+// Determine if selected .c-fold children are assigned the .is-active class
+function is_active(e) {
+  return $(e.currentTarget.children).hasClass("is-active");
+}
+
+function remove_active(e) {
+  debugger;
+  // Remove .is-active from selected .c-fold children
+  $(e.currentTarget.children[0]).removeClass("is-active");
+  $(e.currentTarget.children[1]).removeClass("is-active");
+  $(e.currentTarget.children[2]).removeClass("is-active");
+}
+
+function add_active(e) {
+  // Add .is-active from selected .c-fold children
+  $(e.currentTarget.children[0]).addClass("is-active");
+  $(e.currentTarget.children[1]).addClass("is-active");
+  $(e.currentTarget.children[2]).addClass("is-active");
+}
