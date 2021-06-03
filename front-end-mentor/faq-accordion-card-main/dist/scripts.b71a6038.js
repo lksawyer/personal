e664ea1e6cfec124e76594992e907230ff6186c3
lksawyer <loganksawyer@gmail.com>
2021-06-03 17:45:54 -0400

    change height functionality

--- front-end-mentor/faq-accordion-card-main/dist/scripts.b71a6038.js
+++ front-end-mentor/faq-accordion-card-main/dist/scripts.b71a6038.js
@@ -118,11 +118,18 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"scripts.js":[function(require,module,exports) {
+var text_height = [];
 $(document).ready(function () {
-  console.log("scripts loaded"); // Get the height of all e.currentTarget.children[2]
-  // Store these values
-  // Set e.currentTarget.children[2] css height property to 0
+  console.log("scripts loaded"); // Get the height of all .c-fold__text elements
+  // Store these values in an array
+  // Add data attribute used to ref text_height index
+  // Set height to 0
 
+  $(".c-fold__text").each(function (index) {
+    text_height.push($(this).height());
+    $(this).attr("data-fold", index);
+    $(this).height(0);
+  });
   $(".c-fold").click(function (e) {
     console.log(".c-fold clicked");
     console.log("e :", e);
@@ -130,26 +137,41 @@ $(document).ready(function () {
 
     if (active) {
       remove_active(e);
+      remove_height(e);
     } else {
       add_active(e);
+      add_height(e);
     }
   });
-}); // Determine if selected .c-fold children are assigned the .is-active class
+}); // Set css height property to its original value
+
+function add_height(e) {
+  // Store the index value of the selected .c-fold__text
+  var current_index = $(e.currentTarget.children[2]).attr("data-fold");
+  $(e.currentTarget.children[2]).height(text_height[current_index]);
+} // Set css height property to 0
+
+
+function remove_height(e) {
+  // Store the index value of the selected .c-fold__text
+  var current_index = $(e.currentTarget.children[2]).attr("data-fold");
+  $(e.currentTarget.children[2]).height(0);
+} // Determine if selected .c-fold children are assigned the .is-active class
+
 
 function is_active(e) {
   return $(e.currentTarget.children).hasClass("is-active");
-}
+} // Remove .is-active from selected .c-fold children
+
 
 function remove_active(e) {
-  debugger; // Remove .is-active from selected .c-fold children
-
   $(e.currentTarget.children[0]).removeClass("is-active");
   $(e.currentTarget.children[1]).removeClass("is-active");
   $(e.currentTarget.children[2]).removeClass("is-active");
-}
+} // Add .is-active from selected .c-fold children
+
 
 function add_active(e) {
-  // Add .is-active from selected .c-fold children
   $(e.currentTarget.children[0]).addClass("is-active");
   $(e.currentTarget.children[1]).addClass("is-active");
   $(e.currentTarget.children[2]).addClass("is-active");
@@ -182,7 +204,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53322" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56633" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- front-end-mentor/faq-accordion-card-main/src/scripts.js
+++ front-end-mentor/faq-accordion-card-main/src/scripts.js
@@ -1,9 +1,16 @@
+let text_height = [];
+
 $(document).ready(function () {
   console.log("scripts loaded");
-
-  // Get the height of all e.currentTarget.children[2]
-  // Store these values
-  // Set e.currentTarget.children[2] css height property to 0
+  // Get the height of all .c-fold__text elements
+  // Store these values in an array
+  // Add data attribute used to ref text_height index
+  // Set height to 0
+  $(".c-fold__text").each(function (index) {
+    text_height.push($(this).height());
+    $(this).attr("data-fold", index);
+    $(this).height(0);
+  });
 
   $(".c-fold").click(function (e) {
     console.log(".c-fold clicked");
@@ -13,27 +20,42 @@ $(document).ready(function () {
     // Determine whether or not .is-active should be removed or added
     if (active) {
       remove_active(e);
+      remove_height(e);
     } else {
       add_active(e);
+      add_height(e);
     }
   });
 });
+
+// Set css height property to its original value
+function add_height(e) {
+  // Store the index value of the selected .c-fold__text
+  let current_index = $(e.currentTarget.children[2]).attr("data-fold");
+  $(e.currentTarget.children[2]).height(text_height[current_index]);
+}
+
+// Set css height property to 0
+function remove_height(e) {
+  // Store the index value of the selected .c-fold__text
+  let current_index = $(e.currentTarget.children[2]).attr("data-fold");
+  $(e.currentTarget.children[2]).height(0);
+}
 
 // Determine if selected .c-fold children are assigned the .is-active class
 function is_active(e) {
   return $(e.currentTarget.children).hasClass("is-active");
 }
 
+// Remove .is-active from selected .c-fold children
 function remove_active(e) {
-  debugger;
-  // Remove .is-active from selected .c-fold children
   $(e.currentTarget.children[0]).removeClass("is-active");
   $(e.currentTarget.children[1]).removeClass("is-active");
   $(e.currentTarget.children[2]).removeClass("is-active");
 }
 
+// Add .is-active from selected .c-fold children
 function add_active(e) {
-  // Add .is-active from selected .c-fold children
   $(e.currentTarget.children[0]).addClass("is-active");
   $(e.currentTarget.children[1]).addClass("is-active");
   $(e.currentTarget.children[2]).addClass("is-active");
