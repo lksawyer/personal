d3f65af8579b2fe6a859ad8f94d0d621942d17b0
lksawyer <loganksawyer@gmail.com>
2021-06-10 23:21:55 -0400

    notes

--- mosh/the-ultimate-js-mastery-p2/index.js
+++ mosh/the-ultimate-js-mastery-p2/index.js
@@ -1,1 +1,36 @@
-console.log("Hello World");
+// Factory function
+function createCricle(radius) {
+  return {
+    // With ES6 if key/value are the same, we can use this syntax
+    // - prio to ES6, this code would be radius: radius,
+    radius,
+    draw: function () {
+      console.log("draw");
+    },
+  };
+}
+
+// Call the factory function to create a new circle
+const circle = createCricle(1);
+// Access circle obj members using dot notation
+circle.draw();
+
+// Obj literal syntax ={};
+// - simple way to define an obj
+const circle = {
+  // Properties
+  // - used to hold values
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  // Method
+  // - used to define logic
+  draw: function () {
+    console.log("draw");
+  },
+};
+
+// Access circle obj members using dot notation
+circle.draw();
