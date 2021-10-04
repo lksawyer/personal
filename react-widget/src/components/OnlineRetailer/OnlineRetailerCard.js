6b0be6b277e26325a0c82f9d0baee0650252c82d
lksawyer <loganksawyer@gmail.com>
2021-10-04 16:36:41 -0400

    added retailer logos

--- react-widget/src/components/OnlineRetailer/OnlineRetailerCard.js
+++ react-widget/src/components/OnlineRetailer/OnlineRetailerCard.js
@@ -1,8 +1,14 @@
 import styles from "./OnlineRetailerCard.module.css";
+import heb from "../../assets/img/heb.png";
+import instacart from "../../assets/img/instacart.png";
+import target from "../../assets/img/target.png";
+import walmart from "../../assets/img/walmart.png";
 
 const OnlineRetailerCard = (props) => {
+  // Conditional rendering of stock stock text
   const stock = props.stock ? "In Stock" : "No Stock";
 
+  // Conditional rendering of .online-retailer-card
   if (props.retailer === "") {
     return (
       <div className={styles["online-retailer-card"]}>
@@ -13,10 +19,27 @@ const OnlineRetailerCard = (props) => {
     );
   }
 
+  // Conditional rendering of retailer logo
+  let logo;
+
+  switch (props.retailer) {
+    case "heb":
+      logo = heb;
+      break;
+    case "instacart":
+      logo = instacart;
+      break;
+    case "target":
+      logo = target;
+      break;
+    default:
+      logo = walmart;
+  }
+
   return (
     <div className={styles["online-retailer-card"]}>
       <img
-        src="#"
+        src={logo}
         alt={props.retailer}
         className={styles["online-retailer-card__img"]}
       />
