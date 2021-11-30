28829e834d6e29995fc2aefb3a88257bd203f92f
lksawyer <loganksawyer@gmail.com>
2021-11-30 16:29:07 -0500

    clustering

--- react/projects/google-maps/src/Components/GoogleMap.js
+++ react/projects/google-maps/src/Components/GoogleMap.js
@@ -1,13 +1,21 @@
+import { MarkerClusterer } from '@googlemaps/markerclusterer';
 import { useEffect, useRef } from 'react';
+import config from '../config.json';
 
+// Config
+const color = config.googleMap.marker.color;
+const fillColor = config.googleMap.marker.fillColor;
+
+// Component
 const GoogleMap = ({
   center,
   id,
   localRetailers,
   onLoad,
+  panTo,
+  selectedOffer,
+  setSelectedOffer,
   zoom,
-  setSelectedMarker,
-  selectedMarker,
 }) => {
   // Refs
   const mapRef = useRef();
@@ -26,47 +34,68 @@ const GoogleMap = ({
     });
     infoWindowRef.current.addListener('closeclick', () => {
       console.log('closing');
-      setSelectedMarker(null);
+      setSelectedOffer(null);
     });
     onLoad(map);
-  }, [center.lat, center.lng, onLoad, setSelectedMarker, zoom]);
+  }, [center.lat, center.lng, onLoad, setSelectedOffer, zoom]);
 
-  //Info Window
+  // Info Window
   useEffect(() => {
     console.log('Updating infoWindowRef');
-    console.log('selectedMarker: ', selectedMarker);
-
-    if (selectedMarker) {
-      infoWindowRef.current.setContent(selectedMarker.name);
-      infoWindowRef.current.setPosition({
-        lat: selectedMarker.lat,
-        lng: selectedMarker.lng,
+    if (selectedOffer) {
+      infoWindowRef.current.setOptions({
+        content: selectedOffer.name,
+        pixelOffset: new window.google.maps.Size(0, -25),
+        position: {
+          lat: selectedOffer.lat,
+          lng: selectedOffer.lng,
+        },
       });
+
       infoWindowRef.current.open({
         map: mapRef.current,
         shouldFocus: false,
       });
     }
-  }, [selectedMarker]);
+  }, [selectedOffer]);
 
   // Markers
   useEffect(() => {
     if (mapRef.current) {
       console.log('New marker objects');
-      localRetailers.map((retailer) => {
+      let markers = [];
+
+      localRetailers.map((retailer, i) => {
         const marker = new window.google.maps.Marker({
-          position: { lat: retailer.lat, lng: retailer.lng },
+          icon: {
+            anchor: new window.google.maps.Point(200, 510),
+            fillColor,
+            fillOpacity: 1,
+            labelOrigin: new window.google.maps.Point(205, 190),
+            path: 'M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z',
+            scale: 0.06,
+            strokeColor: '',
+            strokeWeight: 0,
+          },
+          label: {
+            text: (i + 1).toString(),
+            color,
+          },
           map: mapRef.current,
+          position: { lat: retailer.lat, lng: retailer.lng },
         });
 
         marker.addListener('click', () => {
-          setSelectedMarker(retailer);
+          setSelectedOffer(retailer);
+          panTo({ lat: retailer.lat, lng: retailer.lng });
         });
 
-        return null;
+        return markers.push(marker);
       });
+
+      new MarkerClusterer({ markers, map: mapRef.current });
     }
-  }, [localRetailers, setSelectedMarker]);
+  }, [localRetailers, panTo, setSelectedOffer]);
 
   return <div id={id} style={{ width: 500, height: 500 }}></div>;
 };
--- react/projects/google-maps/src/Components/Local.js
+++ react/projects/google-maps/src/Components/Local.js
@@ -4,7 +4,7 @@ import useScriptLoad from '../Hooks/useScriptLoad';
 import GoogleMap from './GoogleMap';
 
 // Temp holder for maker data
-const DUMMY_MARKERS = [
+const DUMMY_OFFERS = [
   {
     name: 'Raleigh',
     lat: 35.779591,
@@ -27,21 +27,16 @@ let center = {
   lat: config.googleMap.center.lat,
   lng: config.googleMap.center.lng,
 };
-
 const clickedZoom = config.googleMap.clickedZoom;
-
 const geolocation = config.googleMap.geolocation;
-
 const options = {
   disableDefaultUI: config.googleMap.options.disableDefaultUI,
   zoomControl: config.googleMap.options.zoomControl,
   fullscreenControl: config.googleMap.options.fullscreenControl,
 };
-
 const zoom = config.googleMap.zoom;
 
-//
-
+// Component
 const Local = () => {
   // Script loader
   let { isLoaded, loadError } = useScriptLoad(
@@ -49,13 +44,30 @@ const Local = () => {
   );
 
   // State
-  const [markers] = useState(DUMMY_MARKERS);
-  const [selectedMarker, setSelectedMarker] = useState(null);
+  const [offers] = useState(DUMMY_OFFERS);
+  const [selectedOffer, setSelectedOffer] = useState(null);
 
-  // Map ref
+  // Refs
   const mapRef = useRef();
+
+  // Map functionality
   const onMapLoad = useCallback((map) => {
     mapRef.current = map;
+    if (geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        mapRef.current.setCenter({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      });
+    }
+  }, []);
+
+  const panTo = useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    if (mapRef.current.zoom !== clickedZoom) {
+      mapRef.current.setZoom(clickedZoom);
+    }
   }, []);
 
   // Dynamic rendering of content based on script load status
@@ -63,17 +75,19 @@ const Local = () => {
   if (!isLoaded) return 'Loading Maps';
 
   return (
-    <div>
+    <>
       <GoogleMap
         center={center}
         id="map"
-        localRetailers={markers}
+        localRetailers={offers}
         onLoad={onMapLoad}
+        options={options}
+        panTo={panTo}
+        selectedOffer={selectedOffer}
+        setSelectedOffer={setSelectedOffer}
         zoom={zoom}
-        setSelectedMarker={setSelectedMarker}
-        selectedMarker={selectedMarker}
       ></GoogleMap>
-    </div>
+    </>
   );
 };
 
