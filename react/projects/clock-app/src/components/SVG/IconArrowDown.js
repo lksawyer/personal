13c4b940e71830d131ca77d80a0268a26a6c1e9a
logan.sawyer <logan.sawyer@channeladvisor.com>
2021-12-23 10:07:50 -0500

    resolved errors for SVGs

--- react/projects/clock-app/src/components/SVG/IconArrowDown.js
+++ react/projects/clock-app/src/components/SVG/IconArrowDown.js
@@ -1,7 +1,7 @@
 const IconArrowDown = () => {
   return (
     <svg width='14' height='9' xmlns='http://www.w3.org/2000/svg'>
-      <path stroke='#FFF' stroke-width='2' fill='none' d='m1 1 6 6 6-6' />
+      <path stroke='#FFF' strokeWidth='2' fill='none' d='m1 1 6 6 6-6' />
     </svg>
   );
 };
