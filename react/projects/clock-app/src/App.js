65f68a28199864e26604c4a293e799ee95dbb69f
logan.sawyer <logan.sawyer@channeladvisor.com>
2021-12-23 17:54:37 -0500

    removed button from clock componenet

--- react/projects/clock-app/src/App.js
+++ react/projects/clock-app/src/App.js
@@ -3,6 +3,7 @@ import Quote from "./components/Quote/Quote";
 import Clock from "./components/Clock/Clock";
 import Expanded from "./components/Expanded/Expanded";
 import Layout from "./components/Layout/Layout";
+import Button from "./components/Button/Button";
 
 function App() {
   return (
--- react/projects/clock-app/src/components/Clock/Clock.js
+++ react/projects/clock-app/src/components/Clock/Clock.js
@@ -2,19 +2,13 @@ import classes from "./Clock.module.css";
 import Greeting from "./Greeting";
 import Time from "./Time";
 import Location from "./Location";
-import Button from "../Button/Button";
 
 const Clock = () => {
   return (
     <div className={classes.Clock}>
-      <div className={classes.Clock__content}>
-        <Greeting />
-        <Time />
-        <Location />
-      </div>
-      <div className={classes.Clock__button}>
-        <Button />
-      </div>
+      <Greeting />
+      <Time />
+      <Location />
     </div>
   );
 };
