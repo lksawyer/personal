a7ec6ee46a32fe4286a8df743b530db9e0fc7390
logan.sawyer <logan.sawyer@channeladvisor.com>
2022-01-04 19:16:58 -0500

    converted unixtime to standard time

--- react/projects/clock-app/src/components/Clock/Time.js
+++ react/projects/clock-app/src/components/Clock/Time.js
@@ -1,13 +1,21 @@
 import classes from "./Time.module.css";
 
 const Time = props => {
-  // Convert time prop to readable time
-  const date = new Date(props.time.datetime).toISOString();
-  // console.log("date: ", date);
+  // Convert unix timestamp to standard time hh:mm
+  const unix_timestamp = props.time.unixtime;
+  const date = new Date(unix_timestamp * 1000);
+  let hours = date.getHours();
+  if (hours > 12) {
+    hours -= 12;
+  }
+  const minutes = "0" + date.getMinutes();
+  const formattedTime = hours + ":" + minutes.substr(-2);
+
+  console.log(formattedTime);
   return (
     <div className={classes.Time}>
       <p className={classes.Time__content}>
-        <span className={classes.Time__hoursMin}>{date}</span>
+        <span className={classes.Time__hoursMin}>{formattedTime}</span>
         <span className={classes.Time__timeZone}>
           {props.time.abbreviation}
         </span>
